1d4539b60af700ee66bbc8716fdaa325b36d6af5
Nikhil <nikhilkokate90@gmail.com>
2025-01-02 11:26:23 +0530

    initial commit

---
+++ src/main/java/com/nk/beststore/BeststoreApplication.java
@@ -0,0 +1,13 @@
+package com.nk.beststore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeststoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeststoreApplication.class, args);
+	}
+
+}
